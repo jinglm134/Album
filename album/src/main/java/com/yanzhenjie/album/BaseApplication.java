c58b484e2924c7794847cbbbbbad43d0e935eb11
jaylm <jinglm134@gmail.com>
2018-01-07 14:06:06 +0800

    no message

---
+++ album/src/main/java/com/yanzhenjie/album/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.yanzhenjie.album;
+
+import android.app.Application;
+
+/**
+ * Created by ${jaylm}
+ * on 2018/1/7.
+ */
+
+public class BaseApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
